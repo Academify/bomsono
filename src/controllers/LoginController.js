0cc7ca123b599a923bf2f7d90cfd93a14829348f
Thiago <thiagofp707@gmail.com>
2022-03-23 22:55:50 -0300

    [feat](Login): Login front email e senha hardcoded

--- src/controllers/LoginController.js
+++ src/controllers/LoginController.js
@@ -2,14 +2,19 @@ import api from '../services/api';
 class LoginController{
   static async login(email, password){
 
-    if(email && password){
-      const response = await api.get('/login', {email, password});
+    if(email === 'email@email.com' && password === '12345'){
+      console.log({email, password});
+      sessionStorage.setItem("login", true);
+      return true;
+      /* const response = await api.get('/login', {email, password});
       if(response.data){
         sessionStorage.setItem("login", true);
         window.location('/');
       }else{
         alert('Email ou senha incorretos!');
-      }
+      } */
+    }else{
+      alert('Email ou senha incorretos!');
     }
   }
 
